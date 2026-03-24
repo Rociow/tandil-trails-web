598a512ccbde09572df190af6098bbcb39b3d399
Rocio <roociow@gmail.com>
2026-03-24 19:49:11 -0300

    feat: listado de senderos con signal reactivo y spinner de carga

--- src/app/features/senderos/sendero-list/sendero-list.ts
+++ src/app/features/senderos/sendero-list/sendero-list.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { SenderoService } from '../../../core/services/sendero';
+import { SenderoResumen } from '../../../core/models/sendero.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-sendero-list',
-  imports: [],
+  imports: [MatCardModule, MatProgressSpinnerModule],
   templateUrl: './sendero-list.html',
   styleUrl: './sendero-list.scss',
 })
-export class SenderoList {}
+export class SenderoList implements OnInit {
+  constructor(private senderoService: SenderoService) { }
+
+  senderos: SenderoResumen[] = [];
+  cargando = signal(true);
+
+  ngOnInit() {
+    this.senderoService.getSenderos().subscribe({
+      next: (response) => {
+  console.log('response:', response);
+  this.senderos = response.content;
+  this.cargando.set(false);
+  console.log('cargando:', this.cargando());
+},
+      error: (err) => {
+        console.error(err);
+          this.cargando.set(false);
+      }
+    });
+  }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-api-en-produccion.com'
+  apiUrl: 'http://localhost:8080'
 };
